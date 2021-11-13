636358c33a19f37c49e6e365259a948d6a24a772
Alexander Nookinov <anookinov@gmail.com>
2021-11-13 11:03:20 +0700

    add setup system to render a sprite

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 use bevy::prelude::*;
 
+const HOTEL_SPRITE: &str = "hotel.png";
+
 fn main() {
-    App::build().run();
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup.system())
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let texture_handle = asset_server.load(HOTEL_SPRITE);
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(texture_handle.into()),
+        ..Default::default()
+    });
 }
